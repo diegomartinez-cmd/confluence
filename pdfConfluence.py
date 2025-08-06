75bf175ad74e3db35b5d1d1a4047f3629c16e5e1
diegomartinez-cmd <diego.martinez@enevasys.com>
2025-08-06 13:58:48 -0600

    Update pdfConfluence.py

--- pdfConfluence.py
+++ pdfConfluence.py
@@ -71,4 +71,8 @@ def index():
     return render_template_string(HTML_FORM)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print("✅ Aplicación Flask iniciada correctamente")
+    app.run(host='0.0.0.0', port=port)
+
+
